71e5c8e6d627782f0621c00b2dd1e9b71cec826d
Roberto Nogueira <enogrob@gmail.com>
2019-08-23 23:04:20 -0300

    performed-exercises

--- src/ch02/useless.erl
+++ src/ch02/useless.erl
@@ -1,5 +1,4 @@
 -module(useless).
--export([add/2, hello/0, greet_and_add_two/1]).
 
 add(A,B) ->
     A + B.
@@ -8,7 +7,7 @@ add(A,B) ->
 %% io:format/1 is the standard function used to output text.
 hello() ->
     io:format("Hello, world!~n").
-	
+
 greet_and_add_two(X) ->
     hello(),
     add(X,2).
